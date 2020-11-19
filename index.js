6a6c0f75d635cbad98bebc2d30f15cd4eba75742
Phil Efstathiou <phil.efstat@gmail.com>
2020-11-19 09:09:26 +0000

    init commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const ip = require('ip')
+const { Kafka } = require('kafkajs')
+
+const host = process.env.HOST_IP || ip.address()
+
+const kafka = new Kafka({
+  brokers: [`${host}:9092`],
+  clientId: 'example-producer',
+})
+
+const producer = kafka.producer()
+const consumer = kafka.consumer({ groupId: 'test-group' })
+
+const run = async () => {
+  // Producing
+  await producer.connect()
+  await producer.send({
+    topic: 'test-topic',
+    messages: [{ value: 'Hello KafkaJS user!' }],
+  })
+
+  // Consuming
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+      })
+    },
+  })
+}
+
+run().catch(console.error)
